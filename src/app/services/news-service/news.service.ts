5f720a8460ae47584516d3e3167806e5a3aa884e
Patricia Vera H <p.vhernandez@hotmail.com>
2020-10-10 02:08:25 +0200

    Added interfaces and classes folders, and news and login services

--- src/app/services/news-service/news.service.ts
+++ src/app/services/news-service/news.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NewsService {
 
-  constructor() { }
+  private newsURL = '';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
 }
