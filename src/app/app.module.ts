d6a9e992713e218ef964c1e77bf2130fdc5d2100
Patricia Vera H <p.vhernandez@hotmail.com>
2020-10-16 00:35:28 +0200

    Article filtering using pipes and navigation buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ArticleGridComponent } from './article-grid/article-grid.component';
 import { ArticleFormComponent } from './article-form/article-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { DialogBodyComponent } from './dialog-body/dialog-body.component';
+import { ArticleFilterPipe } from './pipes/article-filter.pipe';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatCardModule } from '@angular/material/card';
@@ -18,6 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDialogModule } from '@angular/material/dialog';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     ArticleGridComponent,
     ArticleFormComponent,
     LoginFormComponent,
-    DialogBodyComponent
+    DialogBodyComponent,
+    ArticleFilterPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/article-filter.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { isRegExp } from 'util';
+import { ArticleFilterPipe } from './article-filter.pipe';
+
+describe('ArticleFilterPipe', () => {
+    it('create an instance', () => {
+        const pipe = new ArticleFilterPipe();
+        expect(pipe).toBeTruthy();
+    })
+})
